50f694fe5f145b46bac5164394449e75aaf41b07
Juanes <juguevara@unal.edu.co>
2023-09-06 12:54:21 -0500

    Documented code

--- main/main.c
+++ main/main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <esp_log.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
@@ -9,36 +8,43 @@
 #define BLINK_GPIO 2
 #define INTERRUPT_GPIO 13
 
-QueueHandle_t queue1;
+QueueHandle_t shouldBlink;
 
 void blinky(void *pvParameter){
     
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    bool ledBlinking;
     while(1) {
+        xQueueReceive(shouldBlink,&lendBlinking,(TickType_t)10);
         /* Blink off (output low) */
         gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(pdMS_TO_TICKS(200));
+        vTaskDelay(pdMS_TO_TICKS(100));
         /* Blink on (output high) */
-        gpio_set_level(BLINK_GPIO, 1);
-        vTaskDelay(pdMS_TO_TICKS(200));
+        if(ledBlinking)
+            gpio_set_level(BLINK_GPIO, 1);
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
 void isr(void *parameter){
-    gpio_set_level(BLINK_GPIO,1);
+    // Inverts the current state held in the buffer
+    bool ledBlinking;
+    xQueueReceiveFromISR(ledBlinking,&shouldBlink,NULL);
+    ledBlinking = !ledBlinking;
+    xQueueSendFromISR(shouldBlink,&ledBlinking,NULL);
 }
 void app_main()
 {
-    queue1 = xQueueCreate(10,sizeof(bool));
+    shouldBlink = xQueueCreate(1,sizeof(bool));
+
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
     gpio_set_direction(INTERRUPT_GPIO,GPIO_MODE_INPUT);
     gpio_set_pull_mode(INTERRUPT_GPIO,GPIO_PULLUP_ONLY);
 
     gpio_intr_enable(INTERRUPT_GPIO);
-    gpio_set_intr_type(INTERRUPT_GPIO,GPIO_INTR_NEGEDGE);
+    gpio_set_intr_type(INTERRUPT_GPIO,GPIO_INTR_ANYEDGE);
     gpio_install_isr_service(0);
     gpio_isr_handler_add(INTERRUPT_GPIO,isr,NULL);
 
-
-    //xTaskCreate(&blinky, "blinky", 1024,NULL,5,NULL );
+    xTaskCreate(&blinky, "blinky", 1024,NULL,5,NULL );
 }
