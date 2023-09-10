a537b3aa1163dbb34d38b420a37988c2df747b3f
Edward1304 <egoyeneche@unal.edu.co>
2023-09-10 09:31:32 -0500

    Created tash adc LEds potentiometer, new files test

--- main/test.c
+++ main/test.c
@@ -4,15 +4,15 @@
 #include "esp_system.h"
 #include "driver/gpio.h"
 #include "freertos/queue.h"
-#include "drever/adc.h"
+#include "driver/adc.h"
 
 #define BLINK_GPIO 2
 #define INTERRUPT_GPIO 13
 #define LED1_GPIO 2
 #define LED2_GPIO 4
 #define LED3_GPIO 16
-#define POTENCIOMETER_ADC_CHANNEL ADC1_CHANNEL_0
-#define RESOLUTION 1023
+#define POTENIOMETER_ADC_CHANNEL ADC1_CHANNEL_0
+#define RESOLUTION 1023  // Maximun Value Resolution 10 bits
 
 QueueHandle_t ledControl;
 
@@ -21,10 +21,12 @@ void ledControlAdcTask(void *pvParameter){
     uint32_t potentiometerValue = 0;
     
     while(1) {
-        adc_read(&potentiometerValue);
+        adc_read(&potentiometerValue);  // Ready Value potentiometer from ADC
 
+        // settings value potentiometer yo adjust the brightness of the led
         uint32_t scaledValue = (potentiometerValue * 3) / ADC_RESOLUTION;
 
+        //Setting brightness Leds
         gpio_set_level(LED1_GPIO,(scaledValue >=1)? 1 : 0);
         gpio_set_level(LED2_GPIO,(scaledValue >=2)? 1 : 0);
         gpio_set_level(LED3_GPIO,(scaledValue >=1)? 1 : 0);
@@ -39,14 +41,16 @@ void app_main()
 {
     ledControl =xTaskCreate(1,sizeof(uint32_t));
 
+    // Settings pins GPIO LEds
     gpio_set_direction(LED1_GPIO, GPIO_MODE_OUTPUT);
     gpio_set_direction(LED2_GPIO, GPIO_MODE_OUTPUT);
     gpio_set_direction(LED3_GPIO, GPIO_MODE_OUTPUT);
 
+    // setting ADC  for potentiometer
     adc1_config_width(ADC_WIDTH_BIT_10);
-    adc1_config_channel_atten(POTENCIOMETER_ADC_CHANNEL, ADC_ATTEN_DB_0);
+    adc1_config_channel_atten(POTENTIOMETER_ADC_CHANNEL, ADC_ATTEN_DB_0);
 
     
-
+    
     xTaskCreate(&ledControlAdcTask, "led_control_task", 2048,NULL,5,NULL );
 }
