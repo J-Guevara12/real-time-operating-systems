2a8680deb1974f07f7409323ec5c72558bc11727
Juanes <juguevara@unal.edu.co>
2023-09-12 22:42:47 -0500

    Implemented LED fading

---
+++ components/led/led.h
@@ -0,0 +1,17 @@
+#ifndef LED_H
+#define LED_H
+
+#include "driver/ledc.h"
+
+#define LEDC_TIMER              LEDC_TIMER_0
+#define LEDC_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC_OUTPUT_IO          (2) // Define the output GPIO
+#define LEDC_CHANNEL            LEDC_CHANNEL_0
+#define LEDC_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+#define LEDC_FREQUENCY          (5000) // Frequency in Hertz. Set frequency at 5 kHz
+
+void example_ledc_init(void);
+
+void fade(void *pvParameter);
+
+#endif
--- main/main.c
+++ main/main.c
@@ -5,42 +5,25 @@
 #include "driver/gpio.h"
 #include "freertos/queue.h"
 
+#include "led/led.h"
+
 #define BLINK_GPIO 2
 #define BLINK_PERIOD 100
 #define INTERRUPT_GPIO 13
 
 QueueHandle_t shouldBlink;
 
-void blinky(void *pvParameter){
-    
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-    bool state = true;
-    bool sts;
-    
-    while(1) {
-        
-        /* Blink off (output low) */
-        gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(pdMS_TO_TICKS(100));
-        /* Blink on (output high) */
-       
-        if  (xQueueReceive(shouldBlink,&sts,(TickType_t)10) )
-            state = !state;
-        gpio_set_level(BLINK_GPIO, state);
-        vTaskDelay(pdMS_TO_TICKS(100));
-    }
-}
-
 void isr(void *parameter){
     // Inverts the current state held in the buffer
     bool var = false;
     xQueueSendFromISR(shouldBlink,&var,NULL);
 }
+
 void app_main()
 {
     shouldBlink = xQueueCreate(1,sizeof(bool));
+    example_ledc_init();
 
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
     gpio_set_direction(INTERRUPT_GPIO,GPIO_MODE_INPUT);
     gpio_set_pull_mode(INTERRUPT_GPIO,GPIO_PULLUP_ONLY);
 
@@ -49,5 +32,5 @@ void app_main()
     gpio_install_isr_service(0);
     gpio_isr_handler_add(INTERRUPT_GPIO,isr,NULL);
 
-    xTaskCreate(&blinky, "blinky", 1024,NULL,5,NULL );
+    xTaskCreate(&fade, "fade", 1024,NULL,5,NULL );
 }
