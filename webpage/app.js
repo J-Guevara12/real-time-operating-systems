2fd5e8095eb2284bbfec9a95ad487982cb566b96
Edward1304 <egoyeneche@unal.edu.co>
2023-10-08 23:16:08 -0500

    implemented ntp / webpage ntc

--- webpage/app.js
+++ webpage/app.js
@@ -1,119 +1,47 @@
-/**
- * Add gobals here
- */
-var seconds 	= null;
-var otaTimerVar =  null;
 
-/**
- * Initialize functions here.
- */
-$(document).ready(function(){
-	getUpdateStatus();
-});   
+// Elementntos del HTML
+const uartData = document.getElementById('uart-data');
+const temperatureData = document.getElementById('temperature-data');
+const ledCircle = document.getElementById('led-circle');
+const setTimeButton = document.getElementById('set-time');
+const valueInput = document.getElementById('value-input');
+const sendValueButton = document.getElementById('send-value');
 
-/**
- * Gets file name and size for display on the web page.
- */        
-function getFileInfo() 
-{
-    var x = document.getElementById("selected_file");
-    var file = x.files[0];
-
-    document.getElementById("file_info").innerHTML = "<h4>File: " + file.name + "<br>" + "Size: " + file.size + " bytes</h4>";
+// Función para actualizar información UART
+function updateUARTInfo(data) {
+    uartData.textContent = data;
 }
 
-/**
- * Handles the firmware update.
- */
-function updateFirmware() 
-{
-    // Form Data
-    var formData = new FormData();
-    var fileSelect = document.getElementById("selected_file");
+// Función para actualizar temperatura y color del LED
+function updateTemperatureAndLED(temperature) {
+    temperatureData.textContent = temperature + ' °C';
     
-    if (fileSelect.files && fileSelect.files.length == 1) 
-	{
-        var file = fileSelect.files[0];
-        formData.set("file", file, file.name);
-        document.getElementById("ota_update_status").innerHTML = "Uploading " + file.name + ", Firmware Update in Progress...";
-
-        // Http Request
-        var request = new XMLHttpRequest();
-
-        request.upload.addEventListener("progress", updateProgress);
-        request.open('POST', "/OTAupdate");
-        request.responseType = "blob";
-        request.send(formData);
-    } 
-	else 
-	{
-        window.alert('Select A File First')
+    // Cambiar el color del LED en función de la temperatura
+    if (temperature < 30) {
+        ledCircle.style.backgroundColor = 'blue';
+    } else if (temperature < 50) {
+        ledCircle.style.backgroundColor = 'green';
+    } else {
+        ledCircle.style.backgroundColor = 'red';
     }
 }
 
-/**
- * Progress on transfers from the server to the client (downloads).
- */
-function updateProgress(oEvent) 
-{
-    if (oEvent.lengthComputable) 
-	{
-        getUpdateStatus();
-    } 
-	else 
-	{
-        window.alert('total size is unknown')
-    }
-}
+//  datos UART 
+setInterval(() => {
+    const simulatedTemperature = Math.floor(Math.random() * 30) + 20;
+    const simulatedUARTData = 'hola hola';
+    updateUARTInfo(simulatedUARTData);
+    updateTemperatureAndLED(simulatedTemperature);
+}, 5000);  // Actualiza cada 5 segundos (
 
-/**
- * Posts the firmware udpate status.
- */
-function getUpdateStatus() 
-{
-    var xhr = new XMLHttpRequest();
-    var requestURL = "/OTAstatus";
-    xhr.open('POST', requestURL, false);
-    xhr.send('ota_update_status');
+// Boton que aumenta la  frecuencia
+setTimeButton.addEventListener('click', () => {
 
-    if (xhr.readyState == 4 && xhr.status == 200) 
-	{		
-        var response = JSON.parse(xhr.responseText);
-						
-	 	document.getElementById("latest_firmware").innerHTML = response.compile_date + " - " + response.compile_time
+});
 
-		// If flashing was complete it will return a 1, else -1
-		// A return of 0 is just for information on the Latest Firmware request
-        if (response.ota_update_status == 1) 
-		{
-    		// Set the countdown timer time
-            seconds = 10;
-            // Start the countdown timer
-            otaRebootTimer();
-        } 
-        else if (response.ota_update_status == -1)
-		{
-            document.getElementById("ota_update_status").innerHTML = "!!! Upload Error !!!";
-        }
-    }
-}
-
-/**
- * Displays the reboot countdown.
- */
-function otaRebootTimer() 
-{	
-    document.getElementById("ota_update_status").innerHTML = "OTA Firmware Update Complete. This page will close shortly, Rebooting in: " + seconds;
-
-    if (--seconds == 0) 
-	{
-        clearTimeout(otaTimerVar);
-        window.location.reload();
-    } 
-	else 
-	{
-        otaTimerVar = setTimeout(otaRebootTimer, 1000);
-    }
-}
-
+// Recive el valor para modificar la intensidad de LED
+sendValueButton.addEventListener('click', () => {
+    const value = valueInput.value;
+    
+});
 
