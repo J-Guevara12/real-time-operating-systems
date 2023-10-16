44e05499f8bf2f122053a37b1d46204ad973bc0d
Edward1304 <egoyeneche@unal.edu.co>
2023-10-16 14:22:57 -0500

    implemented temperature meter

--- main/main.c
+++ main/main.c
@@ -5,51 +5,137 @@
 
 
 
-void  initializate_sntp(){
-	ESP_LOGI(TAG, "Initializing SNTP");
-    sntp_setoperatingmode(SNTP_OPMODE_POLL);
-    sntp_setservername(0, "pool.ntp.org");
-    sntp_set_time_sync_notification_cb(time_sync_notification_cb);
-#ifdef CONFIG_SNTP_TIME_SYNC_METHOD_SMOOTH
-    sntp_set_sync_mode(SNTP_SYNC_MODE_SMOOTH);
-#endif
-    sntp_init();
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "driver/gpio.h"
+#include "freertos/queue.h"
+
+
+#include "led.h"
+#include "adc.h"
+#include "uart.h"
+#include "thermistor.h"
+
+//-------------------------------------------------------------------------------
+//              DECLARACION DE MEDIR TEMPERATURA
+
+#define INTERRUPT_GPIO 13
+static const char* TAG = "MAIN";
+
+
+// Declaración de colas
+QueueHandle_t brightness;
+QueueHandle_t temperatureQueue; 
+QueueHandle_t uartDataQueue; // cola uart
+QueueHandle_t printSecondsQueue;
+
+// Interuption
+void isr(void *parameter){
+    int msg = false;
+    xQueueSendFromISR(printSecondsQueue,&msg,NULL);
 }
 
-void obtanin_time(){
-    initialize_sntp();
-    //Tiempo de espera para que se establezca el tiempo
-    time_t now = 0;
-    struct tm timeinfo = { 0 };
-    int retry = 0;
-    const int retry_count = 10;
-    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && ++retry < retry_count) {
-        ESP_LOGI(TAG, "Espradno que se establesca la hora... (%d/%d)", retry, retry_count);
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
+void printTemperature(){
+    bool firstTime = true;
+    int wait = 1000;
+    double temp;
+    while(1){
+        if(xQueueReceive(printSecondsQueue,&firstTime,(TickType_t)0)){
+            wait = (wait + 1000)%5000; // Para que mantenga entre 1000 y 5000
+        }
+        xQueueReceive(temperatureQueue,&temp,(TickType_t)10);
+        ESP_LOGI(TAG,"La temperatura es: %f",temp);
+        vTaskDelay(pdMS_TO_TICKS(wait+1000));
     }
-    time(&now);
-    localtime_r(&now, &timeinfo);
-    
 }
 
- void Set_SystemTime_SNTP()  {
 
-	 time_t now;
-	    struct tm timeinfo;
-	    time(&now);
-	    localtime_r(&now, &timeinfo);
-	  
-	    if (timeinfo.tm_year < (2023 - 1900)) {
-	        ESP_LOGI(TAG, "la Hora aun no esta configurada, conectadnod al Wi-Fi y obteniendo hora  por NTP.");
-	        obtain_time();
-	        
-	        time(&now);
-	    }
-}
+//--------------------------------------------------------------------------------
+
+
+//--------------------------------------------------------------------------------
+
+//              DECLARACION E IMPLEMTACION NTP
+
+char Current_Date_Time[100];
+
+
+	void Get_current_date_time(char *date_time){
+		char strftime_buf[64];
+		time_t now;
+			struct tm timeinfo;
+			time(&now);
+			localtime_r(&now, &timeinfo);
+
+				
+					setenv("TZ", "COT-5", 1);
+					tzset();
+					localtime_r(&now, &timeinfo);
+
+					strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+					ESP_LOGI(TAG, "The current date/time in Delhi is: %s", strftime_buf);
+					strcpy(date_time,strftime_buf);
+	}
+
+
+
+	static void initialize_sntp(void){
+		ESP_LOGI(TAG, "Initializing SNTP");
+		sntp_setoperatingmode(SNTP_OPMODE_POLL);
+		sntp_setservername(0, "0.co.pool.ntp.org");
+		sntp_set_time_sync_notification_cb(time_sync_notification_cb);
+	#ifdef CONFIG_SNTP_TIME_SYNC_METHOD_SMOOTH
+		sntp_set_sync_mode(SNTP_SYNC_MODE_SMOOTH);
+	#endif
+		sntp_init();
+	}
+
+	static void obtain_time(void){
+
+
+		initialize_sntp();
+		// wait for time to be set
+		time_t now = 0;
+		struct tm timeinfo = { 0 };
+		int retry = 0;
+		const int retry_count = 10;
+		while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && ++retry < retry_count) {
+			ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
+			vTaskDelay(2000 / portTICK_PERIOD_MS);
+		}
+		time(&now);
+		localtime_r(&now, &timeinfo);
+	}
+
+
+
+
+	void Set_SystemTime_SNTP()  {
+
+		time_t now;
+			struct tm timeinfo;
+			time(&now);
+			localtime_r(&now, &timeinfo);
+		
+			if (timeinfo.tm_year < (2023 - 1900)) {
+				ESP_LOGI(TAG, "la Hora aun no esta configurada, conectadnod al Wi-Fi y obteniendo hora  por NTP.");
+				obtain_time();
+				
+				time(&now);
+			}
+	}
+
+//------------------------------------------------------------------------------
+
 
 
 void app_main(void)
 {
+	//--------------------------------------------------------------------------
+	//               CONEXION WIFI Y NTP
+
+
     // Initialize NVS
 	esp_err_t ret = nvs_flash_init();
 	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
@@ -70,6 +156,46 @@ void app_main(void)
 		Get_current_date_time(Current_Date_Time);
 		printf("Fecha y hora actual = %s\n",Current_Date_Time);
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
-}
+    }
+
+
+    //----------------------------------------------------------------------------
+	//            MEDIR TEMPERATURA
+
+    // Inicialización de la cola para datos UART
+    uartDataQueue = xQueueCreate(1, sizeof(Intensities));
+    temperatureQueue = xQueueCreate(2, sizeof(double));
+    printSecondsQueue = xQueueCreate(1,sizeof(bool));
+
+    // Inicialización de PWM para controlar LEDs  
+    ledc_init_with_pin(R_PIN,0);
+    ledc_init_with_pin(G_PIN,1);
+    ledc_init_with_pin(B_PIN,2);
+
+    //Inicializacion del ADC
+    adc_init();
+
+     //Inicialización UART
+    uart_init();
+
+    //Configutacion de interrupciones (Pines)
+    gpio_set_direction(INTERRUPT_GPIO,GPIO_MODE_INPUT);
+    gpio_set_pull_mode(INTERRUPT_GPIO,GPIO_PULLUP_ONLY);
+
+
+    // Habilitar interrupcion  negatica / flaco de bajada
+    gpio_intr_enable(INTERRUPT_GPIO);
+    gpio_set_intr_type(INTERRUPT_GPIO,GPIO_INTR_NEGEDGE);
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(INTERRUPT_GPIO,isr,NULL);
+
+     // Tareas
+    xTaskCreate(&measureTemperature, "measure Temperature NTC", 2048, NULL, 4, NULL);
+    xTaskCreate(&printTemperature,"Imprimir temperatura cada x segundos",2048,NULL,5,NULL);
+    xTaskCreate(&temperatureSemaphore,"Semaforo de temperatura",2048,NULL,6,NULL);
+    xTaskCreate(&send_to_queue,"Recibe data del UART",2048,NULL,3,NULL);
+    
+
+    ESP_LOGI(TAG,"Finished Task creation");
 }
 
